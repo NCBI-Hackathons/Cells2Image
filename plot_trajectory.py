27d0f41c50d34b27b2e70f3581a34227a741ed17
Brad Busse <bbusse@gmail.com>
2018-03-20 17:08:20 -0400

    Plot features of all images

--- plot_trajectory.py
+++ plot_trajectory.py
@@ -4,6 +4,7 @@ from scipy import ndimage
 import time
 import skimage.draw
 
+
 import image_data
 import image_processing as ip
 
@@ -12,14 +13,15 @@ if __name__ == "__main__":
 
     movgen = image_data.all_movies()
 
+    all_x=[]
+    all_feat=[]
+
     for em,movie in enumerate(movgen):
         framegen = image_data.all_frames(movie)
         x = image_data.synced_times(em)
         features = []
         for ef,frame in enumerate(framegen):
             feat = []
-            #imgplot = plt.imshow(frame[1,:,:])
-            #imgplot.set_cmap('gray')
 
             com_out = np.zeros([frame.shape[1],frame.shape[2]])
             com, labels, numlabels = ip.find_food_vacuole_centroid(frame[1,:,:])
@@ -29,14 +31,22 @@ if __name__ == "__main__":
 
             feat.append(np.sum(frame[0,int(com[0])-50:int(com[0])+50,int(com[1])-50:int(com[1])+50]))
 
-            feat.append(np.sum(frame[1,int(com[0])-50:int(com[0])+50,int(com[1])-50:int(com[1])+50]))
-            #patch,stuff = ip.topolar(frame[0,int(com[0])-50:int(com[0])+50,int(com[1])-50:int(com[1])+50])
-            #plt.imshow(com_out, alpha=0.25, cmap=plt.get_cmap('Oranges'))
+            #feat.append(np.sum(frame[1,int(com[0])-50:int(com[0])+50,int(com[1])-50:int(com[1])+50]))
 
-            #print ef
-            #plt.imshow(patch)
             features.append(feat)
         features = np.array(features)
-        plt.plot(x, features)
+        maxfeat = features.max(axis=0)
+        features = features.astype(np.float) / maxfeat
+
+        #maxfeat = features.max(axis=1)
+
+        #plt.plot(x, norm)
+        #plt.show()
+        #plt.pause(10)
+
+        all_x.append(x)
+        all_feat.append(features)
+
+        plt.plot(x,features)
         plt.draw()
-        plt.pause(10)
+        plt.pause(2)
