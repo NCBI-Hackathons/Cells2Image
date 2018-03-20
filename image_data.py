4c0a4c1a50bce7b27536776dfcce4b796cbdf0de
Brad Busse <bbusse@gmail.com>
2018-03-20 16:33:25 -0400

    Add a function to fetch time-centered x axes

--- image_data.py
+++ image_data.py
@@ -1,36 +1,46 @@
 import skimage.io
 import gc
 import random
+import numpy as np
 
 prefix = '../Curated Images/'
 
 images = [
-            { 'filename':'recording1.czi - recording1.czi #03.tif', 'rounded':69, 'egress':76, 'notes':'' },
-            { 'filename':'recording1.czi - recording1.czi #07.tif', 'rounded':95, 'egress':102, 'notes':'' },
-            { 'filename':'recording1.czi - recording1.czi #09.tif', 'rounded':109, 'egress':114, 'notes':'egress fail' },
-            { 'filename':'recording1.czi - recording1.czi #12.tif', 'rounded':23, 'egress':28, 'notes':'' },
-            { 'filename':'recording1.czi - recording1.czi #13.tif', 'rounded':90, 'egress':100, 'notes':'egress fail' },
-            { 'filename':'recording1.czi - recording1.czi #14.tif', 'rounded':8, 'egress':12, 'notes':'' },
-            { 'filename':'recording1.czi - recording1.czi #17.tif', 'rounded':73, 'egress':81, 'notes':'' },
-            { 'filename':'recording1.czi - recording1.czi #18.tif', 'rounded':9, 'egress':13, 'notes':'' },
-            { 'filename':'recording2.czi - recording2.czi #03.tif', 'rounded':61, 'egress':69, 'notes':'' },
-            { 'filename':'recording2.czi - recording2.czi #04.tif', 'rounded':38, 'egress':42, 'notes':'egress fail' },
-            { 'filename':'recording2.czi - recording2.czi #05.tif', 'rounded':28, 'egress':40, 'notes':''},
-            { 'filename':'recording2.czi - recording2.czi #06.tif', 'rounded':104, 'egress':110, 'notes':'' },
-            { 'filename':'recording2.czi - recording2.czi #09.tif', 'rounded':63, 'egress':69, 'notes':'' },
-            { 'filename':'recording2.czi - recording2.czi #11.tif', 'rounded':26, 'egress':31, 'notes':'' },
-            { 'filename':'recording2.czi - recording2.czi #14.tif', 'rounded':26, 'egress':33, 'notes':'' },
-            { 'filename':'recording2.czi - recording2.czi #15.tif', 'rounded':75, 'egress':82, 'notes':'' },
-            { 'filename':'recording2.czi - recording2.czi #17.tif', 'rounded':73, 'egress':78, 'notes':'' },
-            { 'filename':'recording2.czi - recording2.czi #18.tif', 'rounded':0, 'egress':6, 'notes':'' },
-            { 'filename':'recording2.czi - recording2.czi #20.tif', 'rounded':90, 'egress':96, 'notes':'' },
-            { 'filename':'timelapse.czi - timelapse.czi #10.tif', 'rounded':455, 'egress':463, 'notes':'' },
-            { 'filename':'timelapse.czi - timelapse.czi #12.tif', 'rounded':194, 'egress':198, 'notes':'' },
-            { 'filename':'timelapse.czi - timelapse.czi #13.tif', 'rounded':134, 'egress':139, 'notes':'' },
-            { 'filename':'timelapse.czi - timelapse.czi #16.tif', 'rounded':250, 'egress':254, 'notes':'' },
-            { 'filename':'timelapse.czi - timelapse.czi #19.tif', 'rounded':145, 'egress':151, 'notes':'' },
-            { 'filename':'timelapse.czi - timelapse.czi #20.tif', 'rounded':254, 'egress':259, 'notes':'' }
+            { 'filename':'recording1.czi - recording1.czi #03.tif', 'rounded':69, 'egress':76, 'length':124, 'notes':'' },
+            { 'filename':'recording1.czi - recording1.czi #07.tif', 'rounded':95, 'egress':102, 'length':124, 'notes':'' },
+            { 'filename':'recording1.czi - recording1.czi #09.tif', 'rounded':109, 'egress':114, 'length':124, 'notes':'egress fail' },
+            { 'filename':'recording1.czi - recording1.czi #12.tif', 'rounded':23, 'egress':28, 'length':124, 'notes':'' },
+            { 'filename':'recording1.czi - recording1.czi #13.tif', 'rounded':90, 'egress':100, 'length':124, 'notes':'egress fail' },
+            { 'filename':'recording1.czi - recording1.czi #14.tif', 'rounded':8, 'egress':12, 'length':124, 'notes':'' },
+            { 'filename':'recording1.czi - recording1.czi #17.tif', 'rounded':73, 'egress':81, 'length':124, 'notes':'' },
+            { 'filename':'recording1.czi - recording1.czi #18.tif', 'rounded':9, 'egress':13, 'length':124, 'notes':'' },
+            { 'filename':'recording2.czi - recording2.czi #03.tif', 'rounded':61, 'egress':69, 'length':133, 'notes':'' },
+            { 'filename':'recording2.czi - recording2.czi #04.tif', 'rounded':38, 'egress':42, 'length':133, 'notes':'egress fail' },
+            { 'filename':'recording2.czi - recording2.czi #05.tif', 'rounded':28, 'egress':40, 'length':133, 'notes':''},
+            { 'filename':'recording2.czi - recording2.czi #06.tif', 'rounded':104, 'egress':110, 'length':133, 'notes':'' },
+            { 'filename':'recording2.czi - recording2.czi #09.tif', 'rounded':63, 'egress':69, 'length':133, 'notes':'' },
+            { 'filename':'recording2.czi - recording2.czi #11.tif', 'rounded':26, 'egress':31, 'length':133, 'notes':'' },
+            { 'filename':'recording2.czi - recording2.czi #14.tif', 'rounded':26, 'egress':33, 'length':133, 'notes':'' },
+            { 'filename':'recording2.czi - recording2.czi #15.tif', 'rounded':75, 'egress':82, 'length':133, 'notes':'' },
+            { 'filename':'recording2.czi - recording2.czi #17.tif', 'rounded':73, 'egress':78, 'length':133, 'notes':'' },
+            { 'filename':'recording2.czi - recording2.czi #18.tif', 'rounded':0, 'egress':6, 'length':133, 'notes':'' },
+            { 'filename':'recording2.czi - recording2.czi #20.tif', 'rounded':90, 'egress':96, 'length':133, 'notes':'' },
+            { 'filename':'timelapse.czi - timelapse.czi #10.tif', 'rounded':455, 'egress':463, 'length':600, 'notes':'' },
+            { 'filename':'timelapse.czi - timelapse.czi #12.tif', 'rounded':194, 'egress':198, 'length':600, 'notes':'' },
+            { 'filename':'timelapse.czi - timelapse.czi #13.tif', 'rounded':134, 'egress':139, 'length':600, 'notes':'' },
+            { 'filename':'timelapse.czi - timelapse.czi #16.tif', 'rounded':250, 'egress':254, 'length':600, 'notes':'' },
+            { 'filename':'timelapse.czi - timelapse.czi #19.tif', 'rounded':145, 'egress':151, 'length':600, 'notes':'' },
+            { 'filename':'timelapse.czi - timelapse.czi #20.tif', 'rounded':254, 'egress':259, 'length':600, 'notes':'' }
         ]
+
+
+def synced_times(index, event='rounded'):
+    if event == 'rounded':
+        return np.array(range(0,images[index]['length']))-images[index]['rounded']
+    elif event == 'egress':
+        return np.array(range(0,images[index]['length']))-images[index]['egress']
+    else:
+        return np.array(range(0,images[index]['length']))
 
 def all_movies():
     for image in images:
@@ -39,7 +49,7 @@ def all_movies():
         gc.collect()
 
 def load_movie(filename):
-    movie = skimage.io.imread(''.join([prefix,images['filename']]))
+    movie = skimage.io.imread(''.join([prefix,filename]))
     return movie
 
 def all_frames(movie):
@@ -52,3 +62,6 @@ def fetch_random_frame():
     img = skimage.io.imread(''.join([prefix,images[imgnum]['filename']]))
     framenum = random.randint(0,img.shape[0])
     return img[framenum,:,:,:].squeeze()
+
+if __name__ == "__main__":
+    print synced_times(5)
--- locate_cell.py
+++ locate_cell.py
@@ -14,18 +14,22 @@ if __name__ == "__main__":
 
     movgen = image_data.all_movies()
 
-    for movie in movgen:
+    for em,movie in enumerate(movgen):
         framegen = image_data.all_frames(movie)
-        for frame in framegen:
-            imgplot = plt.imshow(frame[1,:,:])
-            imgplot.set_cmap('gray')
+        for ef,frame in enumerate(framegen):
+            #imgplot = plt.imshow(frame[1,:,:])
+            #imgplot.set_cmap('gray')            
 
             com_out = np.zeros([frame.shape[1],frame.shape[2]])
             com, labels, numlabels = ip.find_food_vacuole_centroid(frame[1,:,:])
             #com_out[labels==1] = 1
             rr,cc = skimage.draw.circle(com[0],com[1],10)
-            com_out[rr,cc] = 1            
-            plt.imshow(com_out, alpha=0.25, cmap=plt.get_cmap('Oranges'))
+            com_out[rr,cc] = 1
 
+            patch,stuff = ip.topolar(frame[0,int(com[0])-50:int(com[0])+50,int(com[1])-50:int(com[1])+50])
+            #plt.imshow(com_out, alpha=0.25, cmap=plt.get_cmap('Oranges'))
+
+            print ef
+            plt.imshow(patch)
             plt.draw()
             plt.pause(0.5)
