02acc0b5b5d6bd6bc5e0225ff5384615fdb58a54
Patrick <patrick.allen.fletcher@gmail.com>
2018-03-20 17:45:45 -0400

    testing features

--- PF_scratch.py
+++ PF_scratch.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage
 from skimage import filters
+from skimage import measure
 
 import load_images
 
 import image_processing as ip
 
-ix=0
+ix=1
 movie = skimage.io.imread(''.join([load_images.prefix,load_images.images[ix]['filename']]))
 # print images[ix]['rounded']
 stop=load_images.images[ix]['egress']
@@ -16,14 +17,23 @@ stop=load_images.images[ix]['egress']
 # circ=np.array(movie.shape[0])
 circ=[]
 intens=[]
-for frame in range(stop):
+feature=[]
+# for frame in range(stop):
+for frame in range(movie.shape[0]):
     A=movie[frame,1,:,:]
     Afluo=movie[frame,0,:,:]
 
     B,RP=ip.get_cell_mask(A,(250,250),ptile=75,blur_sigma=15)
     
     circ.append(4*np.pi*RP.area/(RP.perimeter**2))
-    intens.append(np.sum(B*Afluo))
+    # intens.append(np.sum(B*Afluo))
+    intens.append(np.sum(B*Afluo)/np.sum(B))
+
+    L=measure.label(B)
+    props=measure.regionprops(L,A)
+
+    feature.append(props[0].mean_intensity)
+
 
     # plt.subplot(1,2,1)
     # plt.imshow(A,cmap='gray')
@@ -37,7 +47,11 @@ for frame in range(stop):
     #plt.imshow(L,cmap='gray')
 
 
-plt.plot(range,circ)
-# plt.plot(range(stop),circ[:stop])
-# plt.plot(range(stop),intens[:stop])
+# plt.plot(circ)
+plt.subplot(3,1,1)
+plt.plot(range(stop),circ[:stop])
+plt.subplot(3,1,2)
+plt.plot(range(stop),feature[:stop])
+plt.subplot(3,1,3)
+plt.plot(range(stop),intens[:stop])
 plt.show()
